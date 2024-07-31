6a341c0b4d49ae72c3cc9b63de6f2bcbae65273f
Manuel Rojas <duranalejandro661@gmail.com>
2024-07-31 20:32:52 -0300

    Hero Mobile Styles (testing branches)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 
-import Footer from "../components/Footer";
-import Contact from "../components/Contact";
-import Projects from "../components/Projects";
-import About from "../components/About";
+// import Footer from "../components/Footer";
+// import Contact from "../components/Contact";
+// import Projects from "../components/Projects";
+// import About from "../components/About";
 import Hero from "../components/Hero";
-import Nav from "../components/Nav";
+// import Nav from "../components/Nav";
 
-import VideoBackground from "../common/VideoBackground/VideoBackground";
+// import VideoBackground from "../common/VideoBackground/VideoBackground";
 import BacktoTopBtn from "../common/BackToTopBtn/BacktoTopBtn";
 
 function Home() {
@@ -14,14 +14,14 @@ function Home() {
     <div>
       {/* Miscelaneous */}
       <BacktoTopBtn anchorId="#top" />
-      <VideoBackground />
+      {/* <VideoBackground /> */}
       {/* Main Estructure */}
-        <Nav />
+        {/* <Nav /> */}
         <Hero />
-        <About />{/*Section #1*/}
-        <Projects />{/*Section #2*/}
-        <Contact />{/*Section #3*/}
-        <Footer />
+        {/*<About />Section #1*/}
+        {/*<Projects />{/*Section #2*/}
+        {/*<Contact />{/*Section #3*/}
+        {/*<Footer />*/}
 
     </div>
   );

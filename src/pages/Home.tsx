48657adaaaa0d7c401d36230295742c7ef8ef0dc
Manuel Rojas <duranalejandro661@gmail.com>
2024-08-11 14:09:40 -0300

    Setting device checking with a regex test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,21 @@
-import Footer from "../components/Footer";
-import Contact from "../components/Contact";
-import Projects from "../components/Projects";
-import About from "../components/About";
-import Hero from "../components/Hero";
 import Nav from "../components/Nav";
+import MobileNav from "../components/MobileNav";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import MobileProjects from "../components/MobileProjects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 import VideoBackground from "../common/VideoBackground/VideoBackground";
-import BacktoTopBtn from "../common/BackToTopBtn/BacktoTopBtn";
-import MobileProjects from "../components/MobileProjects";
-import MobileNav from "../components/MobileNav";
 
 function Home() {
   const detectDevice = (): string => {
-    if (document.body.clientWidth < 1050) {
-      // TODO: test efficient way to test user's device type
-      // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    const regex = new RegExp(
+      "Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini",
+      "i"
+    );
+    if (regex.test(navigator.userAgent)) {
       return "mobile";
     } else {
       return "desktop";
@@ -23,8 +24,7 @@ function Home() {
 
   return (
     <>
-      {/* Miscelaneous */}
-      <BacktoTopBtn anchorId="#top" />
+
       <VideoBackground />
 
       {/* Main Estructure */}
